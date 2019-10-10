ddf6990a19d95a13a0d0e1de0b58af4f7ad2c2ad
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-10 16:52:54 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,10 @@
 class Dog
   attr_accessor :name, :breed, :age
   @@all = []
-  def intialize()
+  def intialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
     @@all << self.new
   end
 end
